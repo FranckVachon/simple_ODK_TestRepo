de8c5367a47600f4d544e971dd2a9ca889810f9f
FranckVachon <francisvachon.it@gmail.com>
2015-07-09 18:31:57 -0400

    Update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-var logger = require('../logger')
-logger.debugLevel = 'info';
-logger.log('info','deploy.sh');
+
 dt config:set VHOSTS="'$(cat vhost-config.json | tr -d '\n ')'" \
 ALIASES="'$(cat alias-config.json | tr -d '\n ')'" \
 $(cat .env.production | sed 's/\n/ /')
