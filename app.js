17bac84b3b8d74f524fcfea5aa4c05c7d790db4d
FranckVachon <francisvachon.it@gmail.com>
2015-07-10 09:21:22 -0400

    Update app.js

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ var error = require('./controllers/error-handler')
 var app = express()
 /** Logger **/
 var logger = require('./logger')
-//logger.debugLevel = 'info';
-//logger.log('info','Logger is working');
+logger.debugLevel = 'info';
+logger.log('info','Logger is working');
 
 app.use(morgan('dev'))
 
