3e8a2dd71624eafa10c463b5a503c49f78577261
FranckVachon <francisvachon.it@gmail.com>
2015-07-10 09:17:18 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var express = require('express')
 var morgan = require('morgan')
 
 var vhosts = require('./routes/vhosts')
-var github = require('./routes/github')
-var firebase = require('./routes/firebase')
-var gist = require('./routes/gist')
+//var github = require('./routes/github')
+//var firebase = require('./routes/firebase')
+//var gist = require('./routes/gist')
 var aliases = require('./routes/aliases')
 var error = require('./controllers/error-handler')
 var app = express()
@@ -22,13 +22,13 @@ app.get('/', function (req, res) {
 app.use('/', vhosts)
 logger.log('info','vhosts: '+ vhosts);
 
-app.use('/gh/:user/:repo', github)
+//app.use('/gh/:user/:repo', github)
 //logger.log('info','Github: '+ github);
 
 
-app.use('/fb/:appname', firebase)
+//app.use('/fb/:appname', firebase)
 //logger.log('info','Firebase: '+ firebase);
-app.use('/gist/:gist_id', gist)
+//app.use('/gist/:gist_id', gist)
 //logger.log('info','Github: '+ github);
 app.use('/:alias', aliases)
 //logger.log('info','Aliases: '+ aliases);
